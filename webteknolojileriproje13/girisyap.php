d5c5fd6f9f6c44108f7580de7bf1f4f507783077
Burak Akalın <burakakalin2107@gmail.com>
2025-05-24 12:48:14 +0300

    Web Teknolojileri Proje 13
    Tasarım düzenlemeleri yapıldı. 2 adet fotoğraf çıkarıldı.

---
+++ webteknolojileriproje13/girisyap.php
@@ -0,0 +1,20 @@
+<?php
+$correct_username = 'b241210095@sakarya.edu.tr';
+$correct_password = 'b241210095'; 
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username == $correct_username && $password == $correct_password) {
+    
+    echo "<h1>Hoş geldiniz!</h1>";
+    header("refresh:2; url=anasayfa.html");
+    exit();  
+} else {
+    
+    echo "<h1>Hatalı kullanıcı adı veya şifre! Tekrar deneyiniz.</h1>";
+    
+    header("refresh:2; url=index.html"); 
+    exit();
+}
+?>
